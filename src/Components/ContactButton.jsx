8f8e6b0e701f3f53359969303464f4aafad65279
damzylance <wasiuadegoke14@gmail.com>
2024-01-26 14:49:20 -0800

    elevate contact button zindex

--- src/Components/ContactButton.jsx
+++ src/Components/ContactButton.jsx
@@ -2,7 +2,7 @@ import { Image, Text, VStack,Link } from "@chakra-ui/react"
 
 const ContactButton =()=>{
     return (
-        <Link href={"https://wa.me/message/5NRO44DIOIKKH1"} style={{ position: "fixed", bottom: "1%", right: "1%" }}>
+        <Link href={"https://wa.me/message/5NRO44DIOIKKH1"} style={{ position: "fixed", bottom: "1%", right: "1%",zIndex:"99" }}>
         <VStack gap={"0px"}>
           <Text fontSize={"sm"} marginBottom={"-20px"}>Need help?</Text>
         <Image src={"/assets/images/whatsapplogo.png"} width={"80px"} height={"80px"} alt={"Whatsapp Logo"} />
