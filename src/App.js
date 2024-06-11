bb4990ca5ad07d006787a9715a1f2731fb2748ae
damzylance <wasiuadegoke14@gmail.com>
2024-06-11 17:25:54 +0100

    disable GA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import { useEffect } from "react";
 import Router from "./Router";
-import TagManager from "react-gtm-module";
-import ReactGA from "react-ga4";
-import { Provider } from "react-redux";
-ReactGA.initialize(process.env.REACT_APP_GA);
 
 function App() {
-  useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: window.location.search
-        ? window.location.pathname + window.location.search
-        : window.location.pathname,
-    });
-  });
+ 
   return <Router />;
 }
 
