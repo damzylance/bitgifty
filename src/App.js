df885146993338237da6dec7a15416cc00abbc95
damzylance <wasiuadegoke14@gmail.com>
2024-06-21 00:48:07 +0100

    update gtm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { useEffect } from "react";
 import Router from "./Router";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./Theme";
+import store from "./Store/store";
 
 function App() {
  
-  return <Router />;
+  return (<Provider store={store}>
+    <ChakraProvider theme={theme}>
+  <Router />
+  </ChakraProvider>
+  </Provider>)
+  
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -14,11 +14,9 @@ const tagManagerArgs = {
 TagManager.initialize(tagManagerArgs)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <ChakraProvider theme={theme}>
+  
       <App />
-    </ChakraProvider>
-  </Provider>
+   
 );
 
 // If you want to start measuring performance in your app, pass a function
