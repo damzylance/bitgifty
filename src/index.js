f5cf4492184f6ecc0a841be5bdc33213ced361dc
damzylance <wasiuadegoke14@gmail.com>
2024-06-21 01:51:48 +0100

    disable gtm

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import "./Theme/style.css";
 import store from "./Store/store";
 import "./constants/i18n";
 import TagManager from "react-gtm-module";
-const tagManagerArgs = {
-  gtmId: process.env.REACT_APP_GTM_ID
-}
-TagManager.initialize(tagManagerArgs)
+// const tagManagerArgs = {
+//   gtmId: process.env.REACT_APP_GTM_ID
+// }
+// TagManager.initialize(tagManagerArgs)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   
